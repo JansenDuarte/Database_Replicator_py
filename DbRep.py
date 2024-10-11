46a0ec36361edc0b6d71805c9685cabcffa0b103
jansenduarte1 <jansenduarte.dev@gmail.com>
2024-10-11 20:00:48 -0300

    Fix initial character and the use of first id
    Made a few changes to remove the first character in the .csv
    
    Placed a selection to insert or not the first row in the .csv

--- DbRep.py
+++ DbRep.py
@@ -14,35 +14,43 @@ sql_insert_clause = """
 sql_value_clause = """({0})"""
 
 #Place the .csv files in this directory
-dir_name = os.path.dirname(__file__)
-dir_name += '\\DB_Data\\'
+csv_path = os.path.dirname(__file__) + '\\DB_Data\\'
+gen_path = os.path.dirname(__file__) + '\\Generated_SQLs\\'
 
-csv_files = [f for f in os.listdir(dir_name)]
-
+csv_files = [f for f in os.listdir(csv_path)]
 
 column_names = ''
 
 #This makes a pretty good .sql, but you still need to fix wich is int or varchar,
 #remove the initial index and the last comma of the values
 
+useIndex = input("Generate sql with the first index? [y/n]")
+
+if useIndex == 'y' or useIndex == 'Y':
+	useIndex = True
+else:
+     useIndex = False
+
+
 for csv_file in csv_files:
 
     table_name = csv_file.split('.')[0]
-    output_file = open(table_name + '.sql', 'wt')
+    output_file = open(gen_path + table_name + '.sql', 'wt')
 
-    with open(dir_name + csv_file, newline='') as csv_file:
-        reader = csv.reader(csv_file, quotechar='|')
-
+	#	utf-8-sig encoding is used to remove garbage from the start of the file
+    with open(csv_path + csv_file, encoding="utf-8-sig") as csv_file:
+        reader = csv.reader(csv_file, quotechar='"', skipinitialspace= True)
+        
         row_count = 0;
         values = '';
 
         for row in reader:
             if row_count == 0:
-                column_names = ','.join(row)
+                column_names = ','.join(row) if useIndex else (','.join(row).split(',', 1)[1])
                 column = sql_insert_clause.format(db_name, table_name, column_names)
                 output_file.write(column + '\n')
             else:
-                values = ','.join(row)
+                values = ','.join(row) if useIndex else (','.join(row).split(',', 1)[1])
                 values = sql_value_clause.format(values)
                 output_file.write(values + ', \n')
 
