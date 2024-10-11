5a0a98f914da7fff489cd41e3e307790af961c07
jansenduarte1 <jansenduarte.dev@gmail.com>
2024-10-11 19:58:35 -0300

    Fix exemple .sql location
    Just changed a few things around to keep things organized

---
+++ Generated_SQLs/ExempleTableData.sql
@@ -0,0 +1,17 @@
+
+        use DB_Name
+        insert into ExempleTableData
+        (col_0,col_1,col_2,col_3,col_4,col_5,col_6,col_7,col_8,col_9)
+        values
+
+        
+(data,data,data,data,data,data,data,data,data,data), 
+(data,data,data,data,data,data,data,data,data,data), 
+(data,data,data,data,data,data,data,data,data,data), 
+(data,data,data,data,data,data,data,data,data,data), 
+(data,data,data,data,data,data,data,data,data,data), 
+(data,data,data,data,data,data,data,data,data,data), 
+(data,data,data,data,data,data,data,data,data,data), 
+(data,data,data,data,data,data,data,data,data,data), 
+(data,data,data,data,data,data,data,data,data,data), 
+(data,data,data,data,data,data,data,data,data,data), 
